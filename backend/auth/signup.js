b788fc36d9e5ef85dc5021c68dcec17b9b71efc4
https://github.com/pranjalpathak26051998 <pranjalpathak010@gmail.com>
2023-08-20 19:49:55 +0530

    new changes

--- backend/auth/signup.js
+++ backend/auth/signup.js
@@ -6,7 +6,7 @@ let signupUser=async (req,res)=>{
         // let {FirstName,surname,mobileNumber,emailAddress,password,DateOfBirth,Gender}= req.body;
         let data = req.body;
         data.DateOfBirth = moment(data.DateOfBirth, 'DD-MM-YYYY').format('YYYY-MM-DD'); 
-        
+
         let responseData= await signupModel.create(data);
         console.log(responseData);
       return  res.status(201).send({status:true,"the new account":responseData});
@@ -14,7 +14,7 @@ let signupUser=async (req,res)=>{
        return res.status(500).send({status:false,data:error.message});        
     }
 }
-
 module.exports= signupUser;
     
+
 
