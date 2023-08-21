bbdf4d14b300409eeb3eda7a4c97ba6adf990475
https://github.com/pranjalpathak26051998 <pranjalpathak010@gmail.com>
2023-08-21 11:19:21 +0530

    new changes

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,11 @@
 import React from 'react'
-
-const About = () => {
+import './Contact'
+const About = (props) => {
+  const {data}=props;
   return (
     <div>
       <h1>page under contruction</h1>
+      <h4>{data.name}</h4>
     </div>
   )
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import About from './About';
 const Contact = () => {
   return (
     <div>
       <h1>My contact details will be published here</h1>
+      <h3>{<About data={{name:"pranjal pathak"}} />}</h3>
     </div>
   )
 }
